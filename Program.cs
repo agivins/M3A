9f82697a9d02ce84044876de3a4f5e34ac665552
Alexandra Givins <agivins@usf.edu>
2020-09-23 02:17:04 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Method_3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for input 
+            Console.WriteLine("Please enter your name..");
+            /*
+              Use the cath block to validate user input. 
+            */
+            try
+            {
+                // This variable will gather the user input 
+                string input = Console.ReadLine();
+
+                int nameof = int.Parse(input);
+
+
+
+                {
+
+
+                    Console.WriteLine("Hello" + String.Format("  ", int.Parse(input) + "Go Bus!"));
+                    Console.WriteLine("Press any key to exit the program...");
+                    Console.ReadKey(true);
+
+
+                }
+            }// end of try 
+            catch
+            {
+                Console.WriteLine("Please enter your name and try running the program again...");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }// End of catch 
+        }// End of Main 
+    }// End of Class 
+}// End of namesplace
+
