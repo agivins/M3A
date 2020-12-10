e52bbe10be9fffd0f65ea302e8de47f1928f731d
agivins <70180293+agivins@users.noreply.github.com>
2020-12-10 11:10:04 -0800

    Update Program.cs

--- Method_3A/Method_3A/Program.cs
+++ Method_3A/Method_3A/Program.cs
@@ -1,48 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/*ISM 4300 Console Program- Submitted by Alexandra Givins
+ Comments: This code demonstrates the use of Method Statements after getting an input from users. 
+*/
 
-namespace Method_3A
+using System;
+
+
+
+namespace Method
+
 {
+
     class Program
+
     {
-        static void Main(string[] args)
+
+        static void User()
         {
-            // Ask the user for input 
-            Console.WriteLine("Please enter your name..");
-            /*
-              Use the cath block to validate user input. 
-            */
+            // Ask the user for input
+
+            
+
+        }
+        private static void Main(string[] args)
+
+        {
+
+
             try
+
             {
-                // This variable will gather the user input 
+
+                // This variable will gather the user input
+
                 string input = Console.ReadLine();
+                Console.WriteLine("Please enter your First Name...then press Enter " );
+                string name = Console.ReadLine();
+                Console.WriteLine("Please enter your Last Name...then press Enter ");
+                string name1 = Console.ReadLine();
+                //Here is the the user input 
+                Console.WriteLine("Hello"+ " " + name + " " + name1 +" "+ "GoBus!");
 
-                namespace user  = use.Parse(input);
+            }// End of try
 
-
-
-                {
-
-
-                    Console.WriteLine("Hello" + String.Format("  ", int.Parse(input) + "Go Bus!"));
-                    Console.WriteLine("Press any key to exit the program...");
-                    Console.ReadKey(true);
-
-
-                }
-            }// end of try 
+            /*
+  Use the cath block to validate user input.
+  */
             catch
+
             {
-                Console.WriteLine("Please enter your name and try running the program again...");
+                // Pause the program and wait for the user to press a key to end the program
                 Console.WriteLine("Press any key to exit the program...");
+
                 Console.ReadKey(true);
-            }// End of catch 
-        }// End of Main 
-    }// End of Class 
+
+            }// End of catch
+
+        }// End of Main
+
+    }// End of Class
+
 }// End of namesplace
 
+       
 
 
