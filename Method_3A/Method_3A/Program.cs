292199f5e7a4f9242d319e4cfd69628d81b7fbc0
Alexandra Givins <agivins@usf.edu>
2020-12-10 13:09:08 -0800

    Givins3A

---
+++ Method_3A/Method_3A/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for input 
+            Console.WriteLine("Please enter your name..");
+            /*
+              Use the cath block to validate user input. 
+            */
+            try
+            {
+                // This variable will gather the user input 
+                string input = Console.ReadLine();
+
+                namespace user  = use.Parse(input);
+
+
+
+                {
+
+
+                    Console.WriteLine("Hello" + String.Format("  ", int.Parse(input) + "Go Bus!"));
+                    Console.WriteLine("Press any key to exit the program...");
+                    Console.ReadKey(true);
+
+
+                }
+            }// end of try 
+            catch
+            {
+                Console.WriteLine("Please enter your name and try running the program again...");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }// End of catch 
+        }// End of Main 
+    }// End of Class 
+}// End of namesplace
+
+
+
--- Method_3A/Program.cs
+++ Method_3A/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿/*ISM 4300 Console Program- Submitted by Alexandra Givins
+ Comments: This code demonstrates the use of method statements after getting input from users. 
+*/
+
+using System;
 
 namespace Method_3A
 {
-    class Program
+    class Method_3A .Program
     {
+        static void 
+        {
+            Console.WriteLine("Please enter your name..");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Ask the user for input 
+            Console.WriteLine("Please enter your name..");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello" + name + "-" + "Go Bus!");
+            {
+                Console.WriteLine(" The Program enter your name.");
+                Console.WriteLine("Press any key to exit the program...");
+
+            }
+
         }
     }
-}
+}         
+
+
